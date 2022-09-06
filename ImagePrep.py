0833282c2bb91eeb46d1a51add04afe03d6f801f
XingjianL <lixingjian2002@gmail.com>
2022-09-06 19:10:47 -0400

    moved image preprocesses to a separate file

---
+++ ImagePrep.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+class ImagePrep:
+    KMEANSFILTER = [3,  # num of clusters
+                4,  # num of iterations
+                (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0), # criteria
+                cv2.KMEANS_PP_CENTERS]  # flag
+
+    def __init__(self, slice_size = 10, kmeans_filter = KMEANSFILTER):
+        self.slice_size = slice_size
+        self.k, self.iter_num, self.criteria, self.flag = kmeans_filter
+
+    def slice(self, image):
+        arr_size = tuple(int(element / self.slice_size) for element in image.shape)
+        col_array = np.array_split(image, arr_size[0], axis=0)
+        img_array = []
+        for col in col_array:
+            img_array.append(np.array_split(col,arr_size[1],axis=1))
+        return img_array
+
+    def combineRow(self, imgs):
+        combined_img = imgs[0]
+        for img in imgs[1:]:
+            combined_img = np.concatenate((combined_img,img),axis=1)
+        return combined_img
+
+    def combineCol(self, imgs):
+        combined_img = imgs[0]
+        for img in imgs[1:]:
+            combined_img = np.concatenate((combined_img,img),axis=0)
+        return combined_img
+
+    def reduce_image_color(self, image, ncluster = None):
+        img_kmean = image.reshape(-1,3)
+        img_kmean = np.float32(img_kmean)
+        if ncluster is not None:
+            ret,label,center = cv2.kmeans(img_kmean,ncluster,None,self.criteria,self.iter_num,self.flag)
+        else:
+            ret,label,center = cv2.kmeans(img_kmean,self.k,None,self.criteria,self.iter_num,self.flag)
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        res2 = res.reshape((image.shape))
+        return res2, center
+    
+    def localized_color_segmentation(self, img, inter_k = 2, final_k = 20):
+        slice_imgs = self.slice(img)
+        kmeans = slice_imgs.copy()
+        comb_row = [i for i in range(len(slice_imgs))]
+        for i,row in enumerate(slice_imgs):
+            for j,block in enumerate(row):
+                kmeans[i][j], _ = self.reduce_image_color(block, inter_k)
+            comb_row[i] = (self.combineRow(kmeans[i]))
+        combined_filter = self.combineCol(comb_row)
+        combined_filter, colors = self.reduce_image_color(combined_filter, final_k)
+        return combined_filter, colors
+
+    # input: binary image
+    # output: mean and PCA vector, values of the white (non-zero) region
+    # useful for orientation, variance, and location of the white region
+    def binary_PCA(image):                       
+        pca_vector = []
+        #image = cv2.resize(image,IMAGE_SIZE)
+        coords_data = np.array(cv2.findNonZero(image)).T.reshape((2,-1))            # 2 x n matrix of coords [[x1,x2,...],[y1,y2,...]]
+        mean = np.mean(coords_data,axis=1,keepdims=True)                         # center of coords
+        cov_mat = np.cov(coords_data - mean, ddof = 1)              # find covariance
+        pca_val, pca_vector = np.linalg.eig(cov_mat)                # find eigen vectors (also PCA first and second component)
+        return mean, pca_vector, pca_val
+
+    # input: colored image
+    # output: mean and PCA vector, values of the colors in the image
+    # for transforming and compressing the colors, which may be useful for analysis as demonstrated in 3d visualization
+    def color_3d_PCA(image):
+        pca_vector =[]
+
--- visualization.py
+++ visualization.py
@@ -1,68 +1,15 @@
 
-from turtle import color, width
 from mpl_toolkits import mplot3d
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import time
-class ImagePrep:
-    KMEANSFILTER = [3,  # num of clusters
-                4,  # num of iterations
-                (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0), # criteria
-                cv2.KMEANS_PP_CENTERS]  # flag
 
-    def __init__(self, slice_size = 10, kmeans_filter = KMEANSFILTER):
-        self.slice_size = slice_size
-        self.k, self.iter_num, self.criteria, self.flag = kmeans_filter
-
-    def slice(self, image):
-        arr_size = tuple(int(element / self.slice_size) for element in image.shape)
-        col_array = np.array_split(image, arr_size[0], axis=0)
-        img_array = []
-        for col in col_array:
-            img_array.append(np.array_split(col,arr_size[1],axis=1))
-        return img_array
-
-    def combineRow(self, imgs):
-        combined_img = imgs[0]
-        for img in imgs[1:]:
-            combined_img = np.concatenate((combined_img,img),axis=1)
-        return combined_img
-
-    def combineCol(self, imgs):
-        combined_img = imgs[0]
-        for img in imgs[1:]:
-            combined_img = np.concatenate((combined_img,img),axis=0)
-        return combined_img
-
-    def reduce_image_color(self, image, ncluster = None):
-        img_kmean = image.reshape(-1,3)
-        img_kmean = np.float32(img_kmean)
-        if ncluster is not None:
-            ret,label,center = cv2.kmeans(img_kmean,ncluster,None,self.criteria,self.iter_num,self.flag)
-        else:
-            ret,label,center = cv2.kmeans(img_kmean,self.k,None,self.criteria,self.iter_num,self.flag)
-        center = np.uint8(center)
-        res = center[label.flatten()]
-        res2 = res.reshape((image.shape))
-        return res2, center
-    
-    def localized_color_segmentation(self, img, inter_k = 2, final_k = 20):
-        slice_imgs = self.slice(frame)
-        kmeans = slice_imgs.copy()
-        comb_row = [i for i in range(len(slice_imgs))]
-        for i,row in enumerate(slice_imgs):
-            for j,block in enumerate(row):
-                kmeans[i][j], _ = self.reduce_image_color(block, inter_k)
-            comb_row[i] = (self.combineRow(kmeans[i]))
-        combined_filter = self.combineCol(comb_row)
-        combined_filter, colors = self.reduce_image_color(combined_filter, final_k)
-        return combined_filter, colors
-
+import ImagePrep
 class Visualization:
     def __init__(self):
-        self.img_prep = ImagePrep()
+        self.img_prep = ImagePrep.ImagePrep()
         self.fig = plt.figure()
         self.total_plots = 4
         self.subplot_size = math.ceil(math.sqrt(self.total_plots))
@@ -86,9 +33,9 @@ class Visualization:
         self.main_colors = np.concatenate((self.main_colors, colors), axis=0)
         ax = self.fig.add_subplot(self.subplot_size, self.subplot_size, self.current_fig, projection = '3d')
         ax.scatter(self.main_colors[:,0], self.main_colors[:,1],self.main_colors[:,1], facecolors = self.main_colors/255.,)
-        ax.set_xlim(0,255)
-        ax.set_ylim(0,255)
-        ax.set_zlim(0,255)
+        ax.set_xlim(0,200)
+        ax.set_ylim(0,200)
+        ax.set_zlim(0,200)
         self.subplots.append(ax)
         self.current_fig += 1
         return img
@@ -115,8 +62,8 @@ class Visualization:
         if self.frame_counts > iterations:
             self.frame_counts = 0
             plt.clf()
-        if self.main_colors.shape[0] > 100:
-            self.main_colors = np.delete(self.main_colors, np.random.randint(0, 100, size=80), axis = 0)
+        if self.main_colors.shape[0] > 50:
+            self.main_colors = np.delete(self.main_colors, np.random.randint(0, 40, size=25), axis = 0)
 
         #plt.clf()
     
@@ -130,7 +77,7 @@ class Visualization:
 
 source_directory = "/home/xing/TesterCodes/OpenCV/PathProject/Data/manual_path_edited.mp4"
 
-img_prep = ImagePrep()
+img_prep = ImagePrep.ImagePrep()
 img_vis = Visualization()
 
 cap = cv2.VideoCapture(source_directory)
